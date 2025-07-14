922bb7379b298b264f516fe8e0227e047d0796b5
HItmon <hitlabmodv2@gmail.com>
2025-07-15 00:30:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
@@ -12,10 +14,8 @@ res.sendFile(__path + '/pair.html')
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/main.html')
 })
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`YoutTube: @SunShine\nTelegram: SunShine\nGitHub: @DickiAja\nInstsgram: SunShine\n\nServer running on http://localhost:` + PORT)
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`YoutTube: @SunShine\nTelegram: SunShine\nGitHub: @DickiAja\nInstsgram: SunShine\n\nServer running on http://0.0.0.0:` + PORT)
 })
 
 module.exports = app
